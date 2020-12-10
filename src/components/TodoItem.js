fcf73d0ce7beb7e6bed623c4421f4af4d7043bb2
Dhara Patel <dhara.patel95@hotmail.co.uk>
2020-12-10 15:17:10 +0000

    Add styling function that puts a line through completed tasks

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+   getStyle = () => {
+       if(this.props.todo.completed) {
+           return {
+               textDecoration: 'line-through'
+           }
+       } else {
+           return {
+               textDecoration: 'none'
+           }
+       }
+   }
+
     render() {
         return (
-            <div>
+            <div style={this.getStyle()}>
                 <p>{ this.props.todo.title }</p>
             </div>
         )
@@ -15,5 +27,6 @@ export class TodoItem extends Component {
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
 }
+
 
 export default TodoItem
